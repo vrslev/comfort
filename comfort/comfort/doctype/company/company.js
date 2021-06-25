73d50899f000c2b5d3534c8091bedd4c264e8da1
Lev <levwint@gmail.com>
2021-06-25 16:25:05 +0300

    Company: style, index defaults

--- comfort/comfort/doctype/company/company.js
+++ comfort/comfort/doctype/company/company.js
@@ -1,5 +1,5 @@
 frappe.ui.form.on('Company', {
-	company_name: function (frm) {
+	company_name(frm) {
 		var company_name = frm.doc.company_name;
 		var abbr = company_name.match(/\b(\w)/g).join('');
 		frm.set_value('abbr', abbr)
