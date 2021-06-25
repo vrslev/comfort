03c4b7e4dea3488e1dbe1dbb5efef7826213079f
Lev <levwint@gmail.com>
2021-06-25 16:20:04 +0300

    Customer: validate VK URL and extract VK ID, add gender and customer group

--- comfort/comfort/doctype/customer/customer.py
+++ comfort/comfort/doctype/customer/customer.py
@@ -1,5 +1,33 @@
+import re
+from urllib.parse import parse_qs, urlparse
+
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class Customer(Document):
-    pass
+    def validate(self):
+        self.validate_vk_url()
+        self.set_vk_id()
+
+    def validate_vk_url(self):
+        parsed = urlparse(self.vk_url)
+        ok = False
+        if 'vk.com' in parsed.netloc and 'im' in parsed.path:
+            query = parse_qs(parsed.query)
+            if 'sel' in query:
+                ok = True
+
+        if not ok:
+            frappe.throw(_('Wrong VK URL'))
+
+    def set_vk_id(self):
+        if self.vk_url:
+            res = re.findall(r'sel=(\d+)', self.vk_url)
+            if len(res) > 0:
+                self.vk_id = res[0]
+
+        if not self.vk_id:
+            frappe.throw(_('Cannot extract VK ID from URL'))
+
---
+++ comfort/comfort/doctype/customer_group/customer_group.py
@@ -0,0 +1,5 @@
+from frappe.model.document import Document
+
+
+class CustomerGroup(Document):
+    pass
