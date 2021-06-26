f12ea1c13f1799fcaee0bf3873386a656409ed69
Lev <levwint@gmail.com>
2021-06-26 19:00:27 +0300

    fix(Bin): no-attr error

--- comfort/comfort/doctype/bin/bin.py
+++ comfort/comfort/doctype/bin/bin.py
@@ -2,4 +2,8 @@ from frappe.model.document import Document
 
 
 class Bin(Document):
-    pass
+    def before_insert(self):
+        for d in ["reserved_actual", "available_actual", "reserved_purchased",
+                  "available_purchased", "projected"]:
+            if not hasattr(self, d):
+                setattr(self, d, 0)
