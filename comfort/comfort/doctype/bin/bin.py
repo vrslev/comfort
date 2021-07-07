1386e96e1443cf7d10e9e5b1e6bc2eb66ad1f870
Lev <levwint@gmail.com>
2021-07-07 15:34:16 +0300

    Bin: Refactor, add `update_bin` method (this commit is needed for SO and PO ones)

--- comfort/comfort/doctype/bin/bin.py
+++ comfort/comfort/doctype/bin/bin.py
@@ -1,14 +1,31 @@
+import frappe
 from frappe.model.document import Document
+from frappe.utils.data import cint
+
+fields = [
+    "reserved_actual",
+    "available_actual",
+    "reserved_purchased",
+    "available_purchased",
+    "projected",
+]
 
 
 class Bin(Document):
     def before_insert(self):
-        for d in [
-            "reserved_actual",
-            "available_actual",
-            "reserved_purchased",
-            "available_purchased",
-            "projected",
-        ]:
+        for d in fields:
             if not hasattr(self, d):
                 setattr(self, d, 0)
+
+
+def update_bin(item_code, **kwargs):
+    for d in fields:
+        if d not in kwargs:
+            kwargs[d] = 0
+
+    doc = frappe.get_doc("Bin", item_code)
+
+    for key, value in kwargs.items():
+        setattr(doc, key, cint(getattr(doc, key)) + cint(value))
+
+    doc.save()
