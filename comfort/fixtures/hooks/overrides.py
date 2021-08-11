4d87882602e00c3669bd22b6674a46ded8467c62
Lev <mail@vrslev.com>
2021-08-11 13:02:03 +0300

    fix: Remove `export_doc` method in overriden DocType controller

--- comfort/fixtures/hooks/overrides.py
+++ comfort/fixtures/hooks/overrides.py
@@ -6,9 +6,3 @@ class DocType(_DocType):
     def make_controller_template(self):
         """Do not madly create dt.js, test_dt.py files"""
         make_boilerplate("controller._py", self)
-
-    def export_doc(self):
-        """Do not create unnecessary `__init__.py` files"""
-        from frappe.modules.export_file import export_to_files
-
-        export_to_files(record_list=[["DocType", self.name]], create_init=False)
