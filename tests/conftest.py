9febc95e098a3e516d53da138c0606848d1bcc84
Lev <mail@vrslev.com>
2021-08-13 14:35:49 +0300

    test: Except `pymysql.OperationError`

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,18 @@
 from unittest.mock import MagicMock
 
 import pytest
+from pymysql import OperationalError
 
 import frappe
 from frappe.defaults import *
+
+TEST_SITE_NAME = "tests"
 
 
 @pytest.fixture(scope="session")
 def db_instance():
     """Init frappe, connect to database, do nothing on db.commit()"""
-    frappe.init(site="tests", sites_path="../../sites")
+    frappe.init(site=TEST_SITE_NAME, sites_path="../../sites")
     frappe.connect()
     frappe.db.commit = MagicMock()
     yield frappe.db
@@ -17,7 +20,10 @@ def db_instance():
 
 @pytest.fixture(autouse=True)
 def db_transaction(db_instance: frappe.db):
-    """Rollback on db transaction"""
-    db_instance.begin()
+    """Rollback after db transaction"""
+    try:
+        db_instance.begin()
+    except OperationalError as e:
+        pytest.exit(str(e), returncode=1)
     yield db_instance
     db_instance.rollback()
