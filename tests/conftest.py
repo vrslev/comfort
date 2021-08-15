0e7ad34c62135d33d6f809f3b12d4a8cd9407bfa
Lev <mail@vrslev.com>
2021-08-15 17:03:07 +0300

    test: Fix transaction mixing, remove unused import

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,18 +4,18 @@ import pytest
 from pymysql import OperationalError
 
 import frappe
-from frappe.defaults import *
 
 TEST_SITE_NAME = "tests"
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def db_instance():
     """Init frappe, connect to database, do nothing on db.commit()"""
     frappe.init(site=TEST_SITE_NAME, sites_path="../../sites")
     frappe.connect()
     frappe.db.commit = MagicMock()
     yield frappe.db
+    frappe.destroy()
 
 
 @pytest.fixture(autouse=True)
@@ -25,5 +25,6 @@ def db_transaction(db_instance: frappe.db):
         db_instance.begin()
     except OperationalError as e:
         pytest.exit(str(e), returncode=1)
+
     yield db_instance
     db_instance.rollback()
