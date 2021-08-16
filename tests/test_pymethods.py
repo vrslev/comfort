5bd524f2b5505b62edafb8055d9dfb077fbf479b
Lev <mail@vrslev.com>
2021-08-16 19:59:20 +0300

    tests: Check if pymethod is whitelisted for frontend

--- tests/test_pymethods.py
+++ tests/test_pymethods.py
@@ -7,14 +7,15 @@ from comfort.hooks import app_name
 
 def get_py_js_files():
     path = frappe.get_app_path(app_name)
-    all_files: list[str] = []
+    py: list[str] = []
+    js: list[str] = []
     for root, dirs, files in os.walk(path):  # type: ignore
-        all_files.extend(
-            os.path.join(root, f)
-            for f in files
-            if f.endswith(".js") or f.endswith(".py")
-        )
-    return all_files
+        for f in files:
+            if f.endswith(".py"):
+                py.append(os.path.join(root, f))
+            elif f.endswith(".js"):
+                js.append(os.path.join(root, f))
+    return py, js
 
 
 def get_pymethods(files: list[str]):
@@ -31,7 +32,20 @@ def get_pymethods(files: list[str]):
     return all_matches
 
 
-def test_dotted_py_methods_in_code():
-    methods = get_pymethods(get_py_js_files())
-    for match in methods:
-        frappe.get_attr(match)
+def test_dotted_py_methods_in_code():  # TODO: Use builin frappe's whitelist stuff
+    py_files, js_files = get_py_js_files()
+    methods_in_py, methods_in_js = get_pymethods(py_files), get_pymethods(js_files)
+
+    for method in methods_in_py:
+        frappe.get_attr(method)
+
+    for method in methods_in_js:
+        func = frappe.get_attr(method)
+        try:
+            frappe.is_whitelisted(func)
+        except frappe.exceptions.PermissionError as e:
+            new_exception = e
+            args = list(e.args)
+            args.append(method)
+            new_exception.args = args
+            raise new_exception
