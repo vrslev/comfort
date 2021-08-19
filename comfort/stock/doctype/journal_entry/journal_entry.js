7e596820e17c84b1f7276c68c907999696425e40
Lev <mail@vrslev.com>
2021-08-19 14:38:14 +0300

    refactor: Remove Journal Entry doctype

--- comfort/stock/doctype/journal_entry/journal_entry.js
+++
@@ -1,9 +0,0 @@
-frappe.ui.form.on("Journal Entry", {
-  setup: function (frm) {
-    frm.set_query("account", "accounting_entries", function (doc, cdt, cdn) {
-      return {
-        filters: [["Account", "is_group", "=", 0]],
-      };
-    });
-  },
-});
