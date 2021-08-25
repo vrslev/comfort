5045599105950448339266e989fa2c2db029869a
Lev <mail@vrslev.com>
2021-08-25 20:46:57 +0300

    chore: Remove unused exported fixture

--- comfort/hooks.py
+++ comfort/hooks.py
@@ -1,5 +1,5 @@
-from comfort.fixtures.hooks.metadata import load_metadata
-from comfort.fixtures.hooks.queries import get_standard_queries
+from comfort.comfort_core.hooks.metadata import load_metadata
+from comfort.comfort_core.hooks.queries import get_standard_queries
 
 app_name, app_title, app_description, app_publisher = load_metadata()
 
@@ -8,10 +8,9 @@ standard_queries = get_standard_queries(
 )
 
 after_install = "comfort.finance.chart_of_accounts.initialize_accounts"
-boot_session = "comfort.fixtures.hooks.boot.extend_boot_session_with_currency"
-override_doctype_class = {"DocType": "comfort.fixtures.hooks.overrides.DocType"}
+boot_session = "comfort.comfort_core.hooks.boot.extend_boot_session_with_currency"
+override_doctype_class = {"DocType": "comfort.comfort_core.hooks.overrides.DocType"}
 
 app_include_js = "/assets/comfort/js/index.js"
 
-fixtures = ["List View Settings"]
 treeviews = ["Account"]
