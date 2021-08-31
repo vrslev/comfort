ede1d89d34c5247f406e5400da611d62e445185b
Lev <mail@vrslev.com>
2021-08-31 20:29:49 +0300

    feat(Sales Order): Add Delivery Trip on dashboard

--- comfort/transactions/doctype/sales_order/sales_order_dashboard.py
+++ comfort/transactions/doctype/sales_order/sales_order_dashboard.py
@@ -1,9 +1,13 @@
 def get_data():
     return {
         "fieldname": "voucher_no",
-        "non_standard_fieldnames": {"Purchase Order": "sales_order_name"},
+        "non_standard_fieldnames": {
+            "Purchase Order": "sales_order_name",
+            "Delivery Trip": "sales_order",
+        },
         "transactions": [
-            {"items": ["Payment", "Receipt"]},
+            {"items": ["Payment"]},
             {"items": ["Purchase Order"]},
+            {"items": ["Delivery Trip", "Receipt"]},
         ],
     }
