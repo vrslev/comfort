5335cc11ad653f29a2a077853fe424888128f267
Lev <mail@vrslev.com>
2021-09-27 18:23:38 +0300

    chore: Move to setup.py to enable dependabot alerts

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+__version__ = "0.2.0"
+
+setup(
+    name="comfort",
+    version=__version__,
+    description="Lite-weight ERPNext alternative for specific business",
+    author="Lev Vereshchagin",
+    author_email="mail@vrslev.com",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={"": ["*.md", "*.json", "*.txt", "*.css", "*.csv", "*.html", "*.js"]},
+    zip_safe=False,
+    install_requires=[
+        "erpnext-telegram-integration@git+https://github.com/vrslev/erpnext_telegram.git",
+        "ikea-api-wrapped@git+https://github.com/vrslev/ikea-api-wrapped.git@v0.3.7",
+        "python-telegram-bot==13.7",
+        "vk-api==11.9.2",
+    ],
+    extras_require={
+        "dev": [
+            "black==21.8b0",
+            "pre_commit==2.15.0",
+            "pytest==6.2.5",
+            "pytest-cov==2.12.1",
+            "pytest-randomly==3.10.1",
+        ]
+    },
+)
