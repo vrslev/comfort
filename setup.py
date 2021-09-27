de3723b2f3dbc1b9b1a4a1e923c580275ebd9c98
Lev <mail@vrslev.com>
2021-09-27 17:42:22 +0300

    test

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
 from setuptools import setup
 
-setup(name="comfort")
+setup(
+    name="comfort",
+    install_requires=[
+        "erpnext-telegram-integration@git+https://github.com/vrslev/erpnext_telegram.git",
+        "ikea-api-wrapped@git+https://github.com/vrslev/ikea-api-wrapped.git@v0.3.7",
+        "python-telegram-bot==13.7",
+        "vk-api==11.9.2",
+    ],
+)
