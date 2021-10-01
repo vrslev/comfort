ee8fffd8cbfdd1adcbae7e9b78b97bee46d30323
Lev <mail@vrslev.com>
2021-10-01 14:58:25 +0300

    build: Bump ikea-api-wrapped version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     zip_safe=False,
     install_requires=[
         "erpnext-telegram-integration@git+https://github.com/vrslev/erpnext_telegram.git",
-        "ikea-api-wrapped@git+https://github.com/vrslev/ikea-api-wrapped.git@v0.3.8",
+        "ikea-api-wrapped@git+https://github.com/vrslev/ikea-api-wrapped.git@v0.3.10",
         "python-telegram-bot==13.7",
         "vk-api==11.9.4",
     ],
