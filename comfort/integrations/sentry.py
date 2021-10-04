8f6a4defe15ec4cf73fa4c2bcf9583380790ae41
Lev <mail@vrslev.com>
2021-10-04 20:34:02 +0300

    feat: Add Sentry Python integration

---
+++ comfort/integrations/sentry.py
@@ -0,0 +1,55 @@
+import os
+from logging import Logger
+from typing import Callable
+
+import sentry_sdk
+import sentry_sdk.integrations.wsgi
+from sentry_sdk.integrations.logging import ignore_logger
+from sentry_sdk.integrations.redis import RedisIntegration
+from sentry_sdk.integrations.rq import RqIntegration
+from sentry_sdk.integrations.wsgi import SentryWsgiMiddleware
+from werkzeug.wrappers.response import Response
+
+import frappe
+import frappe.app
+from comfort.hooks import app_version
+
+print(f"{__package__}@{app_version}")
+
+
+def _init_sentry(dsn: str):
+    sentry_sdk.init(
+        dsn=dsn,
+        integrations=[RedisIntegration(), RqIntegration()],
+        traces_sample_rate=1.0,
+        release=f"{__package__}@{app_version}",
+    )
+
+
+def _add_wsgi_integration():
+    frappe.app.application = SentryWsgiMiddleware(frappe.app.application)  # type: ignore
+
+
+def _patch_router_exception_handler():
+    def add_frappe_logger_to_disabled():
+        logger_to_disable: Logger = frappe.logger()
+        ignore_logger(logger_to_disable.name)
+
+    old_handle_exception: Callable[[Exception], Response] = frappe.app.handle_exception  # type: ignore
+
+    def handle_exception(e: Exception):
+        add_frappe_logger_to_disabled()  # Ensure noisy Frappe logger doesn't interrupt
+        # Handle error, if there's issue, Sentry's WSGI integration will handle it:
+        response: Response = old_handle_exception(e)
+        # Otherwise, handle it here:
+        sentry_sdk.capture_exception(e)
+        return response
+
+    frappe.app.handle_exception = handle_exception
+
+
+def init():
+    if dsn := os.environ.get("SENTRY_DSN"):
+        _init_sentry(dsn)
+        _add_wsgi_integration()
+        _patch_router_exception_handler()
