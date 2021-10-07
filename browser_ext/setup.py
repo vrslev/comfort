505b9241f767506d105c608f0c69e06290e47bc4
Lev <mail@vrslev.com>
2021-10-07 20:22:36 +0300

    build(Browser Ext): Dump `ikea_api_wrapped`

--- browser_ext/setup.py
+++ browser_ext/setup.py
@@ -10,7 +10,7 @@ setup(
     zip_safe=False,
     py_modules=["comfort_browser_ext"],
     install_requires=[
-        "ikea-api-wrapped@git+https://github.com/vrslev/ikea-api-wrapped.git@v0.3.10",
+        "ikea-api-wrapped@git+https://github.com/vrslev/ikea-api-wrapped.git@v0.4.0",
         "frappeclient@git+https://github.com/frappe/frappe-client.git@88ed95c472a1ce8ca05c38b29e97cf8c3ece7054#egg=frappeclient",
         "beautifulsoup4==4.10.0",
         "sentry-sdk==1.4.3",
