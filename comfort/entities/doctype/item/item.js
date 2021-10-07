465519037a9d53bdc45b8d32464411b2f2e38b91
Lev <mail@vrslev.com>
2021-10-07 18:25:54 +0300

    fix(Item): Use `frm` instead of `cur_frm`

--- comfort/entities/doctype/item/item.js
+++ comfort/entities/doctype/item/item.js
@@ -1,8 +1,8 @@
 frappe.ui.form.on("Item", {
   refresh(frm) {
     frm.add_custom_button(__("Fetch specs"), () => {
-      comfort.get_items(cur_frm.doc.item_code).then(() => {
-        cur_frm.reload_doc();
+      comfort.get_items(frm.doc.item_code).then(() => {
+        frm.reload_doc();
         frappe.show_alert({
           message: __("Information about item updated"),
           indicator: "Green",
