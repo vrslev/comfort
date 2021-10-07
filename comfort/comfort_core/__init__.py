34b1b7411cc6f7babf608cb005e08beb294dce88
Lev <mail@vrslev.com>
2021-10-07 18:25:40 +0300

    fix(IKEA): Translate text

--- comfort/comfort_core/__init__.py
+++ comfort/comfort_core/__init__.py
@@ -1,5 +1,5 @@
 import frappe
-from comfort import get_all
+from comfort import _, get_all
 from comfort.comfort_core.ikea import fetch_items
 from comfort.entities.doctype.item.item import Item
 
@@ -9,7 +9,9 @@ def get_items(item_codes: str):  # pragma: no cover
     response = fetch_items(item_codes, force_update=True)
     if response["unsuccessful"]:
         frappe.msgprint(
-            "Эти товары не удалось загрузить: " + ", ".join(response["unsuccessful"])
+            _("Cannot fetch those items: {}").format(
+                ", ".join(response["unsuccessful"])
+            )
         )
     return get_all(
         Item,
