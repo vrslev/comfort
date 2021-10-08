89da92c0488ea68fc71fcc8d0f0011948e824033
Lev <mail@vrslev.com>
2021-10-08 12:50:14 +0300

    feat(Delivery Trip): Validations, updating info
    - Validate orders have To Deliver delivery status
    - Force To Deliver delivery status in filter
    - Fix "Complete" translation
    - Update orders info on validation
    - Remove unreachable validation (installation)
    - Remove get_all docstatus filter when adding receips

--- comfort/stock/doctype/delivery_trip/delivery_trip.py
+++ comfort/stock/doctype/delivery_trip/delivery_trip.py
@@ -1,17 +1,26 @@
 from __future__ import annotations
 
 import re
-from typing import Any, Literal
+from typing import Any, Literal, TypedDict
 from urllib.parse import urlencode
 
 import telegram
 from ikea_api_wrapped import format_item_code
 
 import frappe
-from comfort import TypedDocument, ValidationError, _, get_all, get_doc, maybe_json
+from comfort import (
+    TypedDocument,
+    ValidationError,
+    _,
+    get_all,
+    get_doc,
+    group_by_attr,
+    maybe_json,
+)
 from comfort.comfort_core.doctype.telegram_settings.telegram_settings import (
     send_message,
 )
+from comfort.entities.doctype.customer.customer import Customer
 from comfort.stock.doctype.receipt.receipt import Receipt
 from comfort.transactions.doctype.sales_order.sales_order import SalesOrder
 from comfort.transactions.doctype.sales_order_service.sales_order_service import (
@@ -32,10 +41,75 @@ class DeliveryTrip(TypedDocument):
     def before_submit(self):  # pragma: no cover
         self.set_status()
         self._validate_stops_have_address_and_city()
-        self._validate_orders_have_services()
+        self._validate_orders_have_delivery_services()
 
     def before_cancel(self):  # pragma: no cover
         self.set_status()
+
+    def validate(self):  # pragma: no cover
+        self._validate_delivery_statuses_in_orders()
+        self.update_sales_orders_from_db()
+
+    def _validate_delivery_statuses_in_orders(self):
+        orders = get_all(
+            SalesOrder,
+            fields=("name", "delivery_status"),
+            filters={"name": ("in", (s.sales_order for s in self.stops))},
+        )
+        for order in orders:
+            if order.delivery_status == "To Deliver":
+                continue
+            raise ValidationError(
+                _(
+                    "Can only deliver Sales Orders that have delivery status To Deliver: {}"
+                ).format(order.name)
+            )
+
+    def update_sales_orders_from_db(self):
+        order_names = [s.sales_order for s in self.stops]
+        grouped_orders_with_customer = group_by_attr(
+            data=get_all(
+                SalesOrder,
+                fields=("name", "customer", "pending_amount"),
+                filters={"name": ("in", order_names)},
+            ),
+            attr="name",
+        )
+
+        # + 1 loop to be able to get customer additional info later
+        for stop in self.stops:
+            sales_order = grouped_orders_with_customer[stop.sales_order][0]
+            stop.customer = sales_order.customer
+            stop.pending_amount = sales_order.pending_amount
+
+        grouped_services = group_by_attr(
+            data=get_all(
+                SalesOrderService,
+                fields=("parent", "type"),
+                filters={"parent": ("in", order_names)},
+            ),
+            attr="parent",
+        )
+        grouped_customers = group_by_attr(
+            data=get_all(
+                Customer,
+                fields=("name", "address", "city", "phone"),
+                filters={"name": ("in", (s.customer for s in self.stops))},
+            ),
+            attr="name",
+        )
+
+        for stop in self.stops:
+            services = _get_delivery_and_installation_from_services(
+                grouped_services[stop.sales_order]
+            )
+            stop.delivery_type = services["delivery_type"]
+            stop.installation = services["installation"]
+
+            customer = grouped_customers[stop.customer][0]
+            stop.address = customer.address
+            stop.city = customer.city
+            stop.phone = customer.phone
 
     def set_status(self):
         docstatus_to_status_map: dict[
@@ -50,18 +124,11 @@ class DeliveryTrip(TypedDocument):
             if not stop.city:
                 raise ValidationError(_("Every stop should have city"))
 
-    def _validate_orders_have_services(self):
+    def _validate_orders_have_delivery_services(self):
         for stop in self.stops:
-            res = get_delivery_and_installation_for_order(stop.sales_order)
-            if res["delivery_type"] is None:
+            if stop.delivery_type is None:
                 raise ValidationError(
                     _("Sales Order {} has no delivery service").format(stop.sales_order)
-                )
-            if stop.installation and not res["installation"]:
-                raise ValidationError(
-                    _("Sales Order {} has no installation service").format(
-                        stop.sales_order
-                    )
                 )
 
     def _get_template_context(self):
@@ -106,7 +173,6 @@ class DeliveryTrip(TypedDocument):
                 filters={
                     "voucher_type": "Sales Order",
                     "voucher_no": ("in", (s.sales_order for s in self.stops)),
-                    "docstatus": ("!=", 2),
                 },
             )
         ]
@@ -144,14 +210,11 @@ def _get_items_for_order(sales_order_name: str):  # pragma: no cover
     ]
 
 
-@frappe.whitelist()
-def get_delivery_and_installation_for_order(sales_order_name: str):
+def _get_delivery_and_installation_from_services(services: list[SalesOrderService]):
     delivery_type: str | None = None
     installation = False
 
-    for service in get_all(
-        SalesOrderService, fields="type", filters={"parent": sales_order_name}
-    ):
+    for service in services:
         if service.type == "Delivery to Apartment":
             delivery_type = "To Apartment"
         if service.type == "Delivery to Entrance":
@@ -159,7 +222,21 @@ def get_delivery_and_installation_for_order(sales_order_name: str):
         elif service.type == "Installation":
             installation = True
 
-    return {"delivery_type": delivery_type, "installation": installation}
+    class GetDeliveryAndInstallationFromServicesResponse(TypedDict):
+        delivery_type: Literal["To Apartment", "To Entrance"] | None
+        installation: bool
+
+    return GetDeliveryAndInstallationFromServicesResponse(
+        delivery_type=delivery_type, installation=installation
+    )
+
+
+@frappe.whitelist()
+def get_delivery_and_installation_for_order(sales_order_name: str):
+    services = get_all(
+        SalesOrderService, fields="type", filters={"parent": sales_order_name}
+    )
+    return _get_delivery_and_installation_from_services(services)
 
 
 def _prepare_message_for_telegram(message: str):
