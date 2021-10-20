f2c95c81878118fce9c76a2d4d28d3971363553d
Lev <mail@vrslev.com>
2021-10-21 00:25:27 +0300

    fix(Translations): Don't load translations in other languages

--- comfort/translations/__init__.py
+++ comfort/translations/__init__.py
@@ -1,8 +1,11 @@
 import os
 
+import frappe
 from frappe.translate import get_translation_dict_from_file
 
 
 def get_translated_dict() -> dict[str, str]:
-    path = os.path.join(os.path.dirname(__file__), "ru.csv")
-    return get_translation_dict_from_file(path, "ru", "comfort")
+    if frappe.lang == "ru":
+        path = os.path.join(os.path.dirname(__file__), "ru.csv")
+        return get_translation_dict_from_file(path, "ru", "comfort")
+    return {}
