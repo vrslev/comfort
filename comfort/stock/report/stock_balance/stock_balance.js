2ce7a33848bf2eeb93bedd840b1166a81ea5c8e1
Lev <mail@vrslev.com>
2021-11-02 20:08:06 +0300

    fix: Stock Balance report item code align

--- comfort/stock/report/stock_balance/stock_balance.js
+++ comfort/stock/report/stock_balance/stock_balance.js
@@ -5,8 +5,11 @@ frappe.query_reports["Stock Balance"] = {
       label: __("Stock Type"),
       fieldtype: "Select",
       options:
-        "\nAvailable Purchased\nReserved Purchased\nReserved Actual\nAvailable Actual",
+        "\nAvailable Purchased\nAvailable Actual\nReserved Purchased\nReserved Actual",
       reqd: 1,
     },
   ],
+  after_datatable_render() {
+    $(".dt-cell--col-1").css("text-align", "left");
+  },
 };
