9ceba323eb83852c94d7cfa8637ed044460cf0bb
Lev <mail@vrslev.com>
2021-11-06 19:11:40 +0300

    refactor(Browser Ext): Install script

--- browser_ext/install.sh
+++ browser_ext/install.sh
@@ -1,18 +1,17 @@
 set -e
 
-$executable
 dir=~/Library/Services
 
 # Install python package
 python3.9 -m venv $dir/comfort/venv
 
-# Symlink if `-s` is specified
+# Symlink if `--link` is specified
 cp -R comfort.workflow $dir || true
 exec_files="main.js comfort_browser_ext.py setup.py $dir/comfort"
 if [[ $1 == --link ]]; then
-    ln -Ff $exec_files
+  ln -Ff $exec_files
 else
-    cp $exec_files
+  cp $exec_files
 fi
 
 . $dir/comfort/venv/bin/activate
