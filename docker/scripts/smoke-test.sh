aae5f07ec5e2281b4de19a53003cad6a56276bdb
Lev <mail@vrslev.com>
2021-11-09 03:11:46 +0300

    refactor(Docker): Smoke test

--- docker/scripts/smoke-test.sh
+++ docker/scripts/smoke-test.sh
@@ -1,8 +1,9 @@
 set -e
 
-ULINE="\e[1m\e[4m"
-ENDULINE="\e[0m"
-NEWLINE="\n"
+print_group() {
+  echo ::endgroup::
+  echo "::group::$*"
+}
 
 if [ -z $CI ]; then
   DOMAIN=127.0.0.1
@@ -10,37 +11,46 @@ else
   DOMAIN=test.localhost
 fi
 
-function pingSite() {
-  echo -e "${NEWLINE}${ULINE}Ping created site${ENDULINE}"
-  ping_res=$(curl --insecure -sS https://$DOMAIN/api/method/version)
-  echo $ping_res
-  if [[ -z $(echo $ping_res | grep "message" || echo "") ]]; then
-    echo "Ping failed"
-    exit 1
-  fi
+PROJECT_NAME=testcomfort
 
-  echo -e "${NEWLINE}${ULINE}Check Created Site Index Page${ENDULINE}"
-  index_res=$(curl --insecure -sS https://$DOMAIN)
-  if [[ -n $(echo $index_res | grep "Internal Server Error" || echo "") ]]; then
-    echo $index_res
-    echo "Index check failed"
-    exit 1
-  fi
-}
-
-export PROJECT_NAME=testcomfort
-
-echo -e "${NEWLINE}${ULINE}Start Services${ENDULINE}"
-ADMIN_PASSWORD=admin DB_PASSWORD=123 DOMAIN=$DOMAIN LETSENCRYPT_EMAIL=test@example.com \
+# Initial group
+echo ::group::Generate .env
+ADMIN_PASSWORD=admin \
+  DB_PASSWORD=123 \
+  DOMAIN=$DOMAIN \
+  LETSENCRYPT_EMAIL=test@example.com \
   bash scripts/generate-env.sh
+cat .env
 export $(cat .env)
+
+print_group Start services
 docker-compose -p $PROJECT_NAME up -d
 
+print_group Check health
 bash scripts/check-health.sh
-pingSite
+
+
+print_group Ping site
+
+echo Ping version
+ping_res=$(curl --insecure -sS "https://$SITE_NAME/api/method/version")
+echo "$ping_res"
+if [[ -z $(echo "$ping_res" | grep "message" || echo "") ]]; then
+  echo "Ping failed"
+  exit 1
+fi
+
+echo Check index
+index_res=$(curl --insecure -sS "https://$SITE_NAME")
+if [[ -n $(echo "$index_res" | grep "Internal Server Error" || echo "") ]]; then
+  echo "Index check failed"
+  echo "$index_res"
+  exit 1
+fi
+
 
 if [ -z $CI ]; then
-  echo -e "${NEWLINE}${ULINE}Prune Containers${ENDULINE}"
+  print_group Prune containers
   docker-compose -p $PROJECT_NAME down -v --remove-orphans
 fi
 
