e87e92ee6089f7f32f83dd26ee218097e1efe329
Lev <mail@vrslev.com>
2021-11-11 01:04:47 +0300

    fix(Docker): Backup command; upgrade frappe to v13.14.0

--- comfort/hooks.py
+++ comfort/hooks.py
@@ -40,7 +40,7 @@ treeviews = ["Account"]
 
 global_search_doctypes = _get_global_search_doctypes()
 
-reqd_frappe_version = "v13.13.0"
+reqd_frappe_version = "v13.14.0"
 
 scheduler_events = {
     "weekly": [
