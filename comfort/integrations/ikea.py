4d0c1538ee9282efcc35e90d44641f10a6ffa0f7
Lev <mail@vrslev.com>
2021-11-25 14:30:09 +0300

    Fix ikea module

--- comfort/integrations/ikea.py
+++ comfort/integrations/ikea.py
@@ -1,13 +1,18 @@
 from __future__ import annotations
 
-from types import SimpleNamespace
-from typing import Any, TypedDict
+from typing import TypedDict
 
-import ikea_api_wrapped
+import ikea_api
+import ikea_api.wrappers
 import sentry_sdk
-from ikea_api.errors import ItemFetchError, OrderCaptureError
-from ikea_api_wrapped import format_item_code  # type: ignore  # For jenv hook
-from ikea_api_wrapped.types import NoDeliveryOptionsAvailableError, ParsedItem
+from ikea_api import format_item_code as format_item_code  # For jenv hook
+from ikea_api._api import CustomResponse
+from ikea_api.exceptions import (
+    ItemFetchError,
+    NoDeliveryOptionsAvailableError,
+    OrderCaptureError,
+)
+from ikea_api.wrappers import types
 
 import frappe
 from comfort import (
@@ -25,7 +30,6 @@ from comfort.comfort_core.doctype.ikea_settings.ikea_settings import (
     get_authorized_api,
     get_guest_api,
 )
-from comfort.entities.doctype.child_item.child_item import ChildItem
 from comfort.entities.doctype.item.item import Item
 from comfort.entities.doctype.item_category.item_category import ItemCategory
 
@@ -41,12 +45,18 @@ def get_delivery_services(items: dict[str, int]):
     if not zip_code:
         raise ValidationError(_("Enter Zip Code in Ikea Settings"))
     try:
-        return ikea_api_wrapped.get_delivery_services(api, items, zip_code)
+        return ikea_api.wrappers.get_delivery_services(
+            api, items=items, zip_code=zip_code
+        )
     except NoDeliveryOptionsAvailableError:
         frappe.msgprint(_("No available delivery options"), alert=True, indicator="red")
     except OrderCaptureError as exc:
-        if isinstance(exc.args[0], dict):
-            msg = exc.args[0].get("message", "")
+        # TODO: Remove this annotation after ikea_api fix
+        response: CustomResponse = exc.response
+
+        # TODO: Check if this works
+        if isinstance(response._json, dict):
+            msg = response._json.get("message", "")
             if (
                 "Error while connecting to ISOM" in msg
                 or "Cannot read property 'get' of undefined" in msg
@@ -62,12 +72,12 @@ def add_items_to_cart(items: dict[str, int], authorize: bool):
         raise ValidationError(_("No items selected to add to cart"))
 
     api = get_authorized_api() if authorize else get_guest_api()
-    ikea_api_wrapped.add_items_to_cart(api, items)
+    ikea_api.wrappers.add_items_to_cart(api, items=items)
 
 
 @frappe.whitelist()
 def get_purchase_history():
-    return ikea_api_wrapped.get_purchase_history(get_authorized_api())
+    return ikea_api.wrappers.get_purchase_history(get_authorized_api())
 
 
 @frappe.whitelist()
@@ -75,7 +85,9 @@ def get_purchase_info(purchase_id: int, use_lite_id: bool):
     email: str | None = None
     if use_lite_id:
         email = get_cached_value("Ikea Settings", "Ikea Settings", "username")
-    return ikea_api_wrapped.get_purchase_info(get_authorized_api(), purchase_id, email)
+    return ikea_api.wrappers.get_purchase_info(
+        get_authorized_api(), id=str(purchase_id), email=email
+    )
 
 
 def _make_item_category(name: str | None, url: str | None):
@@ -86,63 +98,74 @@ def _make_item_category(name: str | None, url: str | None):
         doc.insert()
 
 
-def _make_items_from_child_items_if_not_exist(parsed_item: ParsedItem):
-    for child_item in parsed_item["child_items"]:
-        if not doc_exists("Item", child_item["item_code"]):
+def _make_items_from_child_items_if_not_exist(parsed_item: types.ParsedItem):
+    for child_item in parsed_item.child_items:
+        if not doc_exists("Item", child_item.item_code):
             doc = new_doc(Item)
-            doc.item_code = child_item["item_code"]
-            doc.item_name = child_item["item_name"]
-            doc.weight = child_item["weight"]
+            doc.item_code = child_item.item_code
+            doc.item_name = child_item.name
+            doc.weight = child_item.weight
             doc.insert()
 
 
-def _child_items_are_same(old_child_items: list[ChildItem], new_child_items: list[Any]):
-    counted_new_child_items = count_qty(
-        SimpleNamespace(item_code=item["item_code"], qty=item["qty"])
-        for item in new_child_items
-    )
-    counted_old_child_items = count_qty(old_child_items)
-    return counters_are_same(counted_new_child_items, counted_old_child_items)
-
-
-def _create_item(parsed_item: ParsedItem):
-    if doc_exists("Item", parsed_item["item_code"]):
-        doc = get_doc(Item, parsed_item["item_code"])
-        doc.item_name = parsed_item["name"]
-        doc.url = parsed_item["url"]
-        doc.rate = parsed_item["price"]
-        doc.weight = parsed_item["weight"]
-        doc.image = parsed_item["image_url"]
-        if not _child_items_are_same(doc.child_items, parsed_item["child_items"]):
+def _create_item(parsed_item: types.ParsedItem):
+    if doc_exists("Item", parsed_item.item_code):
+        doc = get_doc(Item, parsed_item.item_code)
+        doc.item_name = parsed_item.name
+        doc.url = parsed_item.url
+        doc.rate = parsed_item.price
+        doc.weight = parsed_item.weight
+        doc.image = parsed_item.image_url
+        if not counters_are_same(
+            count_qty(doc.child_items), count_qty(parsed_item.child_items)
+        ):
             doc.child_items = []
-            doc.extend("child_items", parsed_item["child_items"])
-        doc.item_categories = []
-        if parsed_item["category_name"]:
-            doc.append(
-                "item_categories", {"item_category": parsed_item["category_name"]}
+            doc.extend(
+                "child_items",
+                [
+                    {
+                        "item_code": i.item_code,
+                        "item_name": i.name,
+                        "weight": i.weight,
+                        "qty": i.qty,
+                    }
+                    for i in parsed_item.child_items
+                ],
             )
+        doc.item_categories = []
+        if parsed_item.category_name:
+            doc.append("item_categories", {"item_category": parsed_item.category_name})
         doc.save()
 
     else:
         doc = new_doc(Item)
-        doc.item_code = parsed_item["item_code"]
-        doc.item_name = parsed_item["name"]
-        doc.url = parsed_item["url"]
-        doc.rate = parsed_item["price"]
-        doc.weight = parsed_item["weight"]
-        doc.image = parsed_item["image_url"]
+        doc.item_code = parsed_item.item_code
+        doc.item_name = parsed_item.name
+        doc.url = parsed_item.url
+        doc.rate = parsed_item.price
+        doc.weight = parsed_item.weight
+        doc.image = parsed_item.image_url
         doc.child_items = []
-        doc.extend("child_items", parsed_item["child_items"])
+        doc.extend(
+            "child_items",
+            [
+                {
+                    "item_code": i.item_code,
+                    "item_name": i.name,
+                    "weight": i.weight,
+                    "qty": i.qty,
+                }
+                for i in parsed_item.child_items
+            ],
+        )
         doc.item_categories = []
-        if parsed_item["category_name"]:
-            doc.append(
-                "item_categories", {"item_category": parsed_item["category_name"]}
-            )
+        if parsed_item.category_name:
+            doc.append("item_categories", {"item_category": parsed_item.category_name})
         doc.insert()
 
 
-def _get_items_to_fetch(item_codes: str | int | list[str], force_update: bool):
-    parsed_item_codes = ikea_api_wrapped.parse_item_codes(item_codes)  # type: ignore
+def _get_items_to_fetch(item_codes: str | list[str], force_update: bool):
+    parsed_item_codes = ikea_api.parse_item_codes(item_codes)
 
     if force_update:
         return parsed_item_codes
@@ -156,19 +179,19 @@ def _get_items_to_fetch(item_codes: str | int | list[str], force_update: bool):
         return [item_code for item_code in parsed_item_codes if item_code not in exist]
 
 
-def _create_item_categories(items: list[ParsedItem]):
+def _create_item_categories(items: list[types.ParsedItem]):
     categories: set[tuple[str | None, str | None]] = set()
     for item in items:
-        categories.add((item["category_name"], item["category_url"]))
+        categories.add((item.category_name, item.category_url))
     for category in categories:
         _make_item_category(*category)
 
 
-def _fetch_child_items(items: list[ParsedItem], force_update: bool):
+def _fetch_child_items(items: list[types.ParsedItem], force_update: bool):
     items_to_fetch: list[str] = []
     for item in items:
-        for child in item["child_items"]:
-            items_to_fetch.append(child["item_code"])
+        for child in item.child_items:
+            items_to_fetch.append(child.item_code)
     return fetch_items(items_to_fetch, force_update=force_update)
 
 
@@ -177,12 +200,13 @@ class FetchItemsResult(TypedDict):
     successful: list[str]
 
 
-def fetch_items(item_codes: str | int | list[str], force_update: bool):
+def fetch_items(item_codes: str | list[str], force_update: bool):
     items_to_fetch = _get_items_to_fetch(item_codes, force_update)
     if not items_to_fetch:
         return FetchItemsResult(unsuccessful=[], successful=[])
 
-    parsed_items = ikea_api_wrapped.get_items(items_to_fetch)
+    # TODO: Remove this type annotation after fix in ikea_api
+    parsed_items: list[types.ParsedItem] = ikea_api.wrappers.get_items(items_to_fetch)
 
     _create_item_categories(parsed_items)
     _fetch_child_items(parsed_items, force_update)
@@ -191,7 +215,7 @@ def fetch_items(item_codes: str | int | list[str], force_update: bool):
     for parsed_item in parsed_items:
         _make_items_from_child_items_if_not_exist(parsed_item)
         _create_item(parsed_item)
-        fetched_item_codes.append(parsed_item["item_code"])
+        fetched_item_codes.append(parsed_item.item_code)
 
     return FetchItemsResult(
         successful=[i for i in items_to_fetch if i in fetched_item_codes],
@@ -214,7 +238,7 @@ def get_items(item_codes: str):  # pragma: no cover
             raise
 
         # If error has this format: ItemFetchError(["item_code", ...])
-        if unsuccessful := ikea_api_wrapped.parse_item_codes(e.args[0]):  # type: ignore
+        if unsuccessful := ikea_api.parse_item_codes(e.args[0]):  # type: ignore
             response = FetchItemsResult(unsuccessful=unsuccessful, successful=[])
             sentry_sdk.capture_exception(e)
         else:
--- tests/integrations/test_ikea.py
+++ tests/integrations/test_ikea.py
@@ -1,11 +1,13 @@
+import json
 from types import SimpleNamespace
 from typing import Any
+from unittest.mock import Mock
 
-import ikea_api_wrapped
+import ikea_api.wrappers
 import pytest
-from ikea_api import IkeaApi
-from ikea_api.errors import OrderCaptureError
-from ikea_api_wrapped.types import NoDeliveryOptionsAvailableError, ParsedItem
+from ikea_api import IKEA
+from ikea_api.exceptions import NoDeliveryOptionsAvailableError, OrderCaptureError
+from ikea_api.wrappers.types import ParsedItem
 
 import comfort.integrations.ikea
 import frappe
@@ -19,7 +21,6 @@ from comfort.entities.doctype.item.item import Item
 from comfort.entities.doctype.item_category.item_category import ItemCategory
 from comfort.integrations.ikea import (
     FetchItemsResult,
-    _child_items_are_same,
     _create_item,
     _create_item_categories,
     _fetch_child_items,
@@ -65,15 +66,15 @@ def test_get_delivery_services_ok(monkeypatch: pytest.MonkeyPatch):
 def test_get_delivery_services_no_delivery_options(
     monkeypatch: pytest.MonkeyPatch, ikea_settings: IkeaSettings
 ):
-    def new_mock_delivery_services(api: IkeaApi, items: Any, zip_code: Any):
-        assert api._token == get_guest_api()._token
+    def new_mock_delivery_services(api: IKEA, items: Any, zip_code: Any):
+        assert api.token == get_guest_api().token
         assert zip_code == ikea_settings.zip_code
-        raise NoDeliveryOptionsAvailableError
+        raise NoDeliveryOptionsAvailableError(Mock())
 
     frappe.message_log = []
 
     monkeypatch.setattr(
-        ikea_api_wrapped, "get_delivery_services", new_mock_delivery_services
+        ikea_api.wrappers, "get_delivery_services", new_mock_delivery_services
     )
 
     assert get_delivery_services({"14251253": 1}) is None
@@ -87,13 +88,18 @@ def test_get_delivery_services_no_delivery_options(
 def test_get_delivery_services_internal_ikea_error(
     monkeypatch: pytest.MonkeyPatch, ikea_settings: IkeaSettings, message: str
 ):
-    def new_mock_delivery_services(api: IkeaApi, items: Any, zip_code: Any):
-        assert api._token == get_guest_api()._token
+    class MockResponse:
+        status_code = 200
+        text = json.dumps({"message": message})
+        _json = {"message": message}
+
+    def new_mock_delivery_services(api: IKEA, items: Any, zip_code: Any):
+        assert api.token == get_guest_api().token
         assert zip_code == ikea_settings.zip_code
-        raise OrderCaptureError({"message": message})
+        raise OrderCaptureError(MockResponse())  # type: ignore
 
     monkeypatch.setattr(
-        ikea_api_wrapped, "get_delivery_services", new_mock_delivery_services
+        ikea_api.wrappers, "get_delivery_services", new_mock_delivery_services
     )
     assert get_delivery_services({"14251253": 1}) is None
     assert "Internal IKEA error, try again" in str(frappe.message_log)  # type: ignore
@@ -104,15 +110,20 @@ def test_get_delivery_services_other_error(
 ):
     exp_arg = {"message": "some different error"}
 
-    def new_mock_delivery_services(api: IkeaApi, items: Any, zip_code: Any):
-        assert api._token == get_guest_api()._token
+    class MockResponse:
+        status_code = 200
+        text = json.dumps(exp_arg)
+        _json = exp_arg
+
+    def new_mock_delivery_services(api: IKEA, items: Any, zip_code: Any):
+        assert api.token == get_guest_api().token
         assert zip_code == ikea_settings.zip_code
-        raise OrderCaptureError(exp_arg)
+        raise OrderCaptureError(MockResponse())  # type: ignore
 
     monkeypatch.setattr(
-        ikea_api_wrapped, "get_delivery_services", new_mock_delivery_services
+        ikea_api.wrappers, "get_delivery_services", new_mock_delivery_services
     )
-    with pytest.raises(OrderCaptureError, match=str(exp_arg)):
+    with pytest.raises(OrderCaptureError, match=json.dumps(exp_arg)):
         assert get_delivery_services({"14251253": 1}) is None
 
 
@@ -121,10 +132,10 @@ def test_get_delivery_services_other_error(
 def test_add_items_to_cart_with_items(monkeypatch: pytest.MonkeyPatch, authorize: bool):
     myapi = get_authorized_api() if authorize else get_guest_api()
 
-    def mock_add_items_to_cart(api: IkeaApi, items: Any):
-        assert api._token == myapi._token
+    def mock_add_items_to_cart(api: IKEA, *, items: Any):
+        assert api.token == myapi.token
 
-    monkeypatch.setattr(ikea_api_wrapped, "add_items_to_cart", mock_add_items_to_cart)
+    monkeypatch.setattr(ikea_api.wrappers, "add_items_to_cart", mock_add_items_to_cart)
     add_items_to_cart({"14251253": 1}, authorize)
 
 
@@ -136,12 +147,12 @@ def test_add_items_to_cart_no_items():
 
 @pytest.mark.usefixtures("ikea_settings")
 def test_get_purchase_history(monkeypatch: pytest.MonkeyPatch):
-    def mock_get_purchase_history(api: IkeaApi):
-        assert api._token == get_authorized_api()._token
+    def mock_get_purchase_history(api: IKEA):
+        assert api.token == get_authorized_api().token
         return mock_purchase_history
 
     monkeypatch.setattr(
-        ikea_api_wrapped, "get_purchase_history", mock_get_purchase_history
+        ikea_api.wrappers, "get_purchase_history", mock_get_purchase_history
     )
     assert get_purchase_history() == mock_purchase_history
 
@@ -151,36 +162,36 @@ def test_get_purchase_history(monkeypatch: pytest.MonkeyPatch):
 def test_get_purchase_info(monkeypatch: pytest.MonkeyPatch, use_lite_id: bool):
     exp_purchase_id = 111111110
 
-    def mock_get_purchase_info(api: IkeaApi, purchase_id: int, email: str):
-        assert api._token == get_authorized_api()._token
-        assert purchase_id == exp_purchase_id
+    def mock_get_purchase_info(api: IKEA, *, id: str, email: str):
+        assert api.token == get_authorized_api().token
+        assert id == str(exp_purchase_id)
         if use_lite_id:
             assert email == get_value("Ikea Settings", None, "username")
         else:
             assert email is None
 
-    monkeypatch.setattr(ikea_api_wrapped, "get_purchase_info", mock_get_purchase_info)
+    monkeypatch.setattr(ikea_api.wrappers, "get_purchase_info", mock_get_purchase_info)
     get_purchase_info(exp_purchase_id, use_lite_id)
 
 
 def test_make_item_category_not_exists(parsed_item: ParsedItem):
-    _make_item_category(parsed_item["category_name"], parsed_item["category_url"])
+    _make_item_category(parsed_item.category_name, parsed_item.category_url)
     categories = get_all(ItemCategory, ("category_name", "url"))
-    assert categories[0].category_name == parsed_item["category_name"]
-    assert categories[0].url == parsed_item["category_url"]
+    assert categories[0].category_name == parsed_item.category_name
+    assert categories[0].url == parsed_item.category_url
 
 
 def test_make_item_category_exists(parsed_item: ParsedItem):
-    _make_item_category(parsed_item["category_name"], parsed_item["category_url"])
+    _make_item_category(parsed_item.category_name, parsed_item.category_url)
     _make_item_category(
-        parsed_item["category_name"], "https://www.ikea.com/ru/ru/cat/-43638"
+        parsed_item.category_name, "https://www.ikea.com/ru/ru/cat/-43638"
     )
     categories = get_all(ItemCategory, "url")
-    assert categories[0].url == parsed_item["category_url"]
+    assert categories[0].url == parsed_item.category_url
 
 
 def test_make_item_category_no_name(parsed_item: ParsedItem):
-    _make_item_category(None, parsed_item["category_url"])
+    _make_item_category(None, parsed_item.category_url)
     categories = get_all(ItemCategory, "url")
     assert len(categories) == 0
 
@@ -188,106 +199,80 @@ def test_make_item_category_no_name(parsed_item: ParsedItem):
 def test_make_items_from_child_items_if_not_exist(parsed_item: ParsedItem):
     _make_items_from_child_items_if_not_exist(parsed_item)
     items_in_db = {item.item_code for item in get_all(Item, "item_code")}
-    assert (
-        len({item["item_code"] for item in parsed_item["child_items"]} ^ items_in_db)
-        == 0
-    )
+    assert len({i.item_code for i in parsed_item.child_items} ^ items_in_db) == 0
+
     _make_items_from_child_items_if_not_exist(parsed_item)  # test if not exists block
     items_in_db = {item.item_code for item in get_all(Item, "item_code")}
-    assert (
-        len({item["item_code"] for item in parsed_item["child_items"]} ^ items_in_db)
-        == 0
-    )
-
-
-def test_child_items_are_same_true(item: Item, parsed_item: ParsedItem):
-    assert _child_items_are_same(item.child_items, parsed_item["child_items"])
-
-
-def test_child_items_are_same_false(item: Item, parsed_item: ParsedItem):
-    parsed_item["child_items"][0]["qty"] = 1204
-    assert not _child_items_are_same(item.child_items, parsed_item["child_items"])
+    assert len({i.item_code for i in parsed_item.child_items} ^ items_in_db) == 0
 
 
 def test_create_item_exists(parsed_item: ParsedItem):
-    _make_item_category(parsed_item["category_name"], parsed_item["category_url"])
+    _make_item_category(parsed_item.category_name, parsed_item.category_url)
     _create_item(parsed_item)
 
-    parsed_item["name"] = "My New Fancy Item Name"
-    parsed_item["url"] = "https://www.ikea.com/ru/ru/p/-s29128563"
-    parsed_item["price"] = 10000253
-    parsed_item["category_name"] = "New category"
-    _make_item_category(parsed_item["category_name"], parsed_item["category_url"])
+    parsed_item.name = "My New Fancy Item Name"
+    parsed_item.url = "https://www.ikea.com/ru/ru/p/-s29128563"
+    parsed_item.price = 10000253
+    parsed_item.category_name = "New category"
+    _make_item_category(parsed_item.category_name, parsed_item.category_url)
     _create_item(parsed_item)
 
-    doc = get_doc(Item, parsed_item["item_code"])
+    doc = get_doc(Item, parsed_item.item_code)
 
-    assert doc.item_code == parsed_item["item_code"]
-    assert doc.item_name == parsed_item["name"]
-    assert doc.url == parsed_item["url"]
-    assert doc.rate == parsed_item["price"]
+    assert doc.item_code == parsed_item.item_code
+    assert doc.item_name == parsed_item.name
+    assert doc.url == parsed_item.url
+    assert doc.rate == parsed_item.price
     assert counters_are_same(
-        count_qty(doc.child_items),
-        count_qty(
-            SimpleNamespace(item_code=i["item_code"], qty=i["qty"])
-            for i in parsed_item["child_items"]
-        ),
+        count_qty(doc.child_items), count_qty(parsed_item.child_items)
     )
     assert len(doc.item_categories) == 1
-    assert doc.item_categories[0].item_category == parsed_item["category_name"]
+    assert doc.item_categories[0].item_category == parsed_item.category_name
 
 
 def test_create_item_exists_child_items_changed(parsed_item: ParsedItem):
-    _make_item_category(parsed_item["category_name"], parsed_item["category_url"])
+    _make_item_category(parsed_item.category_name, parsed_item.category_url)
     _create_item(parsed_item)
 
-    parsed_item["child_items"].pop()
+    parsed_item.child_items.pop()
     _create_item(parsed_item)
 
-    doc = get_doc(Item, parsed_item["item_code"])
+    doc = get_doc(Item, parsed_item.item_code)
     assert counters_are_same(
-        count_qty(doc.child_items),
-        count_qty(
-            SimpleNamespace(item_code=i["item_code"], qty=i["qty"])
-            for i in parsed_item["child_items"]
-        ),
+        count_qty(doc.child_items), count_qty(parsed_item.child_items)
     )
 
 
 def test_create_item_not_exists(parsed_item: ParsedItem):
-    _make_item_category(parsed_item["category_name"], parsed_item["category_url"])
+    _make_item_category(parsed_item.category_name, parsed_item.category_url)
     _create_item(parsed_item)
-    doc = get_doc(Item, parsed_item["item_code"])
+    doc = get_doc(Item, parsed_item.item_code)
 
-    assert doc.item_code == parsed_item["item_code"]
-    assert doc.item_name == parsed_item["name"]
-    assert doc.url == parsed_item["url"]
-    assert doc.rate == parsed_item["price"]
-    assert doc.weight == parsed_item["weight"]
+    assert doc.item_code == parsed_item.item_code
+    assert doc.item_name == parsed_item.name
+    assert doc.url == parsed_item.url
+    assert doc.rate == parsed_item.price
+    assert doc.weight == parsed_item.weight
     assert counters_are_same(
-        count_qty(doc.child_items),
-        count_qty(
-            SimpleNamespace(item_code=i["item_code"], qty=i["qty"])
-            for i in parsed_item["child_items"]
-        ),
+        count_qty(doc.child_items), count_qty(parsed_item.child_items)
     )
     assert len(doc.item_categories) == 1
-    assert doc.item_categories[0].item_category == parsed_item["category_name"]
+    assert doc.item_categories[0].item_category == parsed_item.category_name
 
 
 def test_create_item_not_exists_no_child_items(parsed_item: ParsedItem):
-    parsed_item["child_items"] = []
-    _make_item_category(parsed_item["category_name"], parsed_item["category_url"])
+    parsed_item.child_items = []
+    _make_item_category(parsed_item.category_name, parsed_item.category_url)
     _create_item(parsed_item)
-    doc = get_doc(Item, parsed_item["item_code"])
+    doc = get_doc(Item, parsed_item.item_code)
 
     assert len(doc.child_items) == 0
 
 
 def test_create_item_not_exists_no_item_category(parsed_item: ParsedItem):
-    parsed_item["category_name"] = ""
+    parsed_item.category_name = ""
     _create_item(parsed_item)
-    doc = get_doc(Item, parsed_item["item_code"])
+    doc = get_doc(Item, parsed_item.item_code)
 
     assert len(doc.item_categories) == 0
 
@@ -306,10 +291,10 @@ def test_get_items_to_fetch_not_force_update(item: Item):
 def test_create_item_categories(parsed_item: ParsedItem):
     items = [parsed_item, parsed_item.copy(), parsed_item]
     new_category = "New Category Name"
-    items[1]["category_name"] = new_category
+    items[1].category_name = new_category
     _create_item_categories(items)
     categories_in_db = {c.name for c in get_all(ItemCategory)}
-    assert len({new_category, parsed_item["category_name"]} ^ categories_in_db) == 0
+    assert len({new_category, parsed_item.category_name} ^ categories_in_db) == 0
 
 
 @pytest.mark.parametrize("force_update", (True, False))
@@ -325,7 +310,11 @@ def test_fetch_child_items(monkeypatch: pytest.MonkeyPatch, force_update: bool):
     monkeypatch.setattr(comfort.integrations.ikea, "fetch_items", mock_fetch_items)
     assert (
         _fetch_child_items(
-            [{"child_items": [{"item_code": i} for i in exp_child_items]}],  # type: ignore
+            [  # type: ignore
+                SimpleNamespace(
+                    child_items=[SimpleNamespace(item_code=i) for i in exp_child_items]
+                )
+            ],
             force_update,
         )
         == exp_result
@@ -353,7 +342,7 @@ def test_fetch_items_main(monkeypatch: pytest.MonkeyPatch, input_force_update: b
 
     input_item_codes = ["123"]
     items_to_fetch = ["012", "123"]
-    parsed_items = [{"item_code": "123"}]
+    parsed_items = [SimpleNamespace(item_code="123")]
 
     def mock_get_items_to_fetch(item_codes: list[str], force_update: bool):
         assert item_codes == input_item_codes
@@ -392,7 +381,7 @@ def test_fetch_items_main(monkeypatch: pytest.MonkeyPatch, input_force_update: b
     monkeypatch.setattr(
         comfort.integrations.ikea, "_get_items_to_fetch", mock_get_items_to_fetch
     )
-    monkeypatch.setattr(ikea_api_wrapped, "get_items", mock_get_items)
+    monkeypatch.setattr(ikea_api.wrappers, "get_items", mock_get_items)
     monkeypatch.setattr(
         comfort.integrations.ikea,
         "_create_item_categories",
