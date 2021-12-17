14d0464b893d2a4d6e66b0d31b4c3b331b2c80dc
Lev Vereshchagin <mail@vrslev.com>
2021-12-17 10:27:12 +0300

    chore: Refactor compare_ikea_purchases script

--- scripts/compare_ikea_purchases.py
+++ scripts/compare_ikea_purchases.py
@@ -1,76 +1,120 @@
+import argparse
 import re
-from typing import Any
+import sys
+from typing import Any, TypedDict
 
-from ikea_api import IKEA
-
-email = ""
-token = ""  # nosec
+from ikea_api import IKEA, format_item_code
 
 
-def compare_orders(first_no: str, second_no: str):
-    before = get_items_in_order(first_no)
-    after = get_items_in_order(second_no)
-    comparison = get_items_in_comparison(before, after)
-    print_differences(comparison)
+class FetchedItem(TypedDict):
+    item_code: str
+    qty: int
+    price: int
 
 
-def get_items_in_order(order_number: str):
-    api = IKEA(token)
-    o = api.purchases.order_info(
+class MappedItem(TypedDict):
+    quantity: int
+    price: int
+
+
+def get_price(item: dict[str, Any]) -> int:
+    match = re.findall(r"(.*),\d+", item["unitPrice"]["formatted"])[0]
+    return int(re.sub(r"[^\d]+", "", match))
+
+
+def fetch_items_in_purchase(
+    *, ikea: IKEA, email: str, order_number: str
+) -> list[FetchedItem]:
+    purchase = ikea.purchases.order_info(
         queries=["ProductListOrder"],
         email=email,
         order_number=order_number,
-        take_products=1000,
+        take_products=10000000,
+    )
+    raw_items: list[dict[str, Any]] = purchase[0]["data"]["order"]["articles"]["any"]
+    return [
+        {"item_code": item["id"], "qty": item["quantity"], "price": get_price(item)}
+        for item in raw_items
+    ]
+
+
+def get_items_map(items: list[FetchedItem]):
+    res: dict[str, MappedItem] = {}
+    for item in items:
+        item_code = item["item_code"]
+        if item_code not in res:
+            res[item_code] = {"quantity": 0, "price": item["price"]}
+        res[item_code]["quantity"] += item["qty"]
+    return res
+
+
+def compare_items(before: list[FetchedItem], after: list[FetchedItem]):
+    map_before = get_items_map(before)
+    res: dict[str, tuple[MappedItem, MappedItem]] = {}
+
+    for item_code, item_after in get_items_map(after).items():
+        item_before = map_before.get(item_code)
+        if item_before and item_after["price"] != item_before["price"]:
+            res[item_code] = item_before, item_after
+    return res
+
+
+def print_differences(
+    *,
+    first_purchase_no: str,
+    second_purchase_no: str,
+    item_comparison: dict[str, tuple[MappedItem, MappedItem]],
+):
+    print(f"Сравнение цен в заказах {first_purchase_no} и {second_purchase_no}.\n")
+
+    full_diff = 0
+    for item_code, (item_before, item_after) in item_comparison.items():
+        diff = (
+            item_after["quantity"] * item_after["price"]
+            - item_before["quantity"] * item_before["price"]
+        )
+        full_diff += diff
+        print(
+            f"{format_item_code(item_code)}. До: {item_before['quantity']} × {item_before['price']} ₽."
+            + f" После: {item_after['quantity']} × {item_after['price']} ₽. Разница: {diff} ₽"
+        )
+
+    print(f"\nОбщая разница: {full_diff} ₽.")
+
+
+def fetch_and_compare_orders(
+    *, ikea: IKEA, email: str, first_purchase_no: str, second_purchase_no: str
+):
+    before = fetch_items_in_purchase(
+        ikea=ikea, email=email, order_number=first_purchase_no
+    )
+    after = fetch_items_in_purchase(
+        ikea=ikea, email=email, order_number=second_purchase_no
+    )
+    item_comparison = compare_items(before, after)
+    print_differences(
+        first_purchase_no=first_purchase_no,
+        second_purchase_no=second_purchase_no,
+        item_comparison=item_comparison,
     )
 
-    items_raw = o[0]["data"]["order"]["articles"]["any"]
-    items: list[dict[str, Any]] = []
-    for i in items_raw:
-        item = {
-            "item_code": i["id"],
-            "qty": i["quantity"],
-            "price": int(
-                re.sub(
-                    r"[^\d]+",
-                    "",
-                    re.findall(r"(.*),\d+", i["unitPrice"]["formatted"])[0],
-                )
-            ),
-        }
-        items.append(item)
-    return items
+
+def main(_args: list[str]) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("first_purchase_no")
+    parser.add_argument("second_purchase_no")
+    parser.add_argument("--email", required=True)
+    args = parser.parse_args(_args)
+    ikea = IKEA()
+    ikea.login_as_guest()
+    fetch_and_compare_orders(
+        ikea=ikea,
+        email=args.email,
+        first_purchase_no=args.first_purchase_no,
+        second_purchase_no=args.second_purchase_no,
+    )
+    return 0
 
 
-def get_map(d: list[Any]):
-    m: dict[Any, Any] = {}
-    for v in d:
-        code = v["item_code"]
-        if code not in m:
-            m[code] = {"quantity": 0, "price": v["price"]}
-        m[code]["quantity"] += v["qty"]
-    return m
-
-
-def get_items_in_comparison(before: list[Any], after: list[Any]):
-    map_before = get_map(before)
-    map_after = get_map(after)
-
-    comparison: dict[Any, Any] = {}
-    for k, v in map_after.items():
-        if k in map_before:
-            diff: int = v["price"] - map_before[k]["price"]
-            if diff > 0:
-                comparison[k] = map_before[k], map_after[k]
-    return comparison
-
-
-def print_differences(comparison: dict[Any, Any]):
-    differences: list[Any] = []
-    for b, a in comparison.values():
-        differences.append(a["quantity"] * a["price"] - b["quantity"] * b["price"])
-
-    for k, (b, a) in comparison.items():
-        print(
-            f"{k}. До: {b['quantity']} x {b['price']} ₽. После: {a['quantity']} x {a['price']} ₽. Разница: {a['quantity'] * a['price'] - b['quantity'] * b['price']} ₽"
-        )
-    print("Общая сумма:", sum(differences), "₽")
+if __name__ == "__main__":
+    raise SystemExit(main(sys.argv[1:]))
