932780516f494123d4bf8cf718184e5a12b46589
Lev Vereshchagin <mail@vrslev.com>
2021-12-22 11:38:35 +0300

    Remove preload

--- comfort/app.py
+++ comfort/app.py
@@ -1,3 +1,4 @@
+import os
 import os.path
 from typing import Any
 
@@ -6,25 +7,12 @@ from werkzeug.middleware.shared_data import SharedDataMiddleware
 
 import frappe
 import frappe.app
-from frappe.boot import get_bootinfo
-from frappe.utils.bench_helper import get_sites
-
-
-def _get_first_site():
-    sites: list[str] = get_sites(None)  # type: ignore
-    assert sites, "No sites found"
-    return sites[0]
 
 
 def _get_asgi_app():
-    first_site = _get_first_site()
     _app: Any = frappe.app.application
 
-    with frappe.init_site(first_site):
-        # Load bootinfo so first startup is faster
-        frappe.connect()
-        get_bootinfo()
-
+    with frappe.init_site():
         # Some code so uvicorn could be used in development:
         # - Set current site to first site (like `bench serve does`)
         # - Add /assets and /files endpoint
@@ -38,7 +26,6 @@ def _get_asgi_app():
                     "/files": os.path.abspath(sites_path),
                 },
             )
-            frappe.app._site = first_site
 
     return WSGIMiddleware(_app)
 
