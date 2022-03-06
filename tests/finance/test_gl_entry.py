ae70d41aff302a39a61d087fcbd49b7dc2f8b7b1
Lev Vereshchagin <mail@vrslev.com>
2022-03-06 14:33:42 +0300

    test: Refactor GL Entry test

--- tests/finance/test_gl_entry.py
+++ tests/finance/test_gl_entry.py
@@ -4,9 +4,8 @@ from comfort.finance.doctype.gl_entry.gl_entry import GLEntry
 from frappe import ValidationError
 
 
-@pytest.mark.parametrize("v", ("Assets", "Liabilities", "Expense", "Income", "Service"))
-def test_gl_entry_validate_raises(gl_entry: GLEntry, v: str):
-    gl_entry.account = v
+def test_gl_entry_validate_raises(gl_entry: GLEntry):
+    gl_entry.account = "Assets"
     with pytest.raises(ValidationError, match="Can't add GL Entry for group account"):
         gl_entry.validate()
 
