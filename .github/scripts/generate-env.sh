c5ab66765aa8f5e2baa711f9d6208615220aeacd
Lev Vereshchagin <mail@vrslev.com>
2022-03-14 11:54:44 +0300

    Add test

--- .github/scripts/generate-env.sh
+++ .github/scripts/generate-env.sh
@@ -1,7 +1,7 @@
 set -e
 
 echo "\
-FRAPPE_VERSION=$(awk -F '"' '/reqd_frappe_version/{print $2}' ../comfort/hooks.py)
+FRAPPE_VERSION=$(awk -F '"' '/reqd_frappe_version/{print $2}' ./comfort/hooks.py)
 COMFORT_VERSION=$COMFORT_VERSION
 DB_PASSWORD=$DB_PASSWORD
 LETSENCRYPT_EMAIL=$LETSENCRYPT_EMAIL
---
+++ .github/scripts/smoke-test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+export DB_PASSWORD=123
+export LETSENCRYPT_EMAIL=mail@example.com
+export DOMAIN=127.0.0.1
+
+./.github/scripts/generate-env.sh
+
+docker-compose exec backend bench new-site $DOMAIN --install-app comfort --db-root-password $DB_PASSWORD --admin-password admin
+
+echo Ping version
+ping_res=$(curl --insecure -sS "https://$DOMAIN/api/method/version")
+echo "$ping_res"
+if [[ -z $(echo "$ping_res" | grep "message" || echo "") ]]; then
+  echo "Ping failed"
+  exit 1
+fi
+
+echo Check index
+index_res=$(curl --insecure -sS "https://$DOMAIN")
+if [[ -n $(echo "$index_res" | grep "Internal Server Error" || echo "") ]]; then
+  echo "Index check failed"
+  echo "$index_res"
+  exit 1
+fi
